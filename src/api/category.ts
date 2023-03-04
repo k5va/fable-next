9479e697d6ce94e8ee0f80ef822bba42ab666b69
k5va <k5va@yandex.ru>
2023-03-04 15:03:22 +0500

    Fixes api bugs

--- src/api/category.ts
+++ src/api/category.ts
@@ -4,11 +4,15 @@ import { ApiRoute, BACKEND_URL } from "./api.const";
 import axios from "axios";
 
 export async function fetchCategories(): Promise<Category[]> {
-  const data = await axios.get(`${BACKEND_URL}/${ApiRoute.CATEGORY}`);
+  const { data } = await axios.get<Category[]>(
+    `${BACKEND_URL}/${ApiRoute.CATEGORY}`
+  );
   return categorySchema.array().parseAsync(data);
 }
 
 export async function fetchCategory(id: string): Promise<Category> {
-  const data = await axios.get(`${BACKEND_URL}/${ApiRoute.CATEGORY}/${id}`);
+  const { data } = await axios.get<Category>(
+    `${BACKEND_URL}/${ApiRoute.CATEGORY}/${id}`
+  );
   return categorySchema.parseAsync(data);
 }
--- src/api/collection.ts
+++ src/api/collection.ts
@@ -4,11 +4,15 @@ import { ApiRoute, BACKEND_URL } from "./api.const";
 import axios from "axios";
 
 export async function fetchCollections(): Promise<Collection[]> {
-  const data = await axios.get(`${BACKEND_URL}/${ApiRoute.COLLECTION}`);
+  const { data } = await axios.get<Collection[]>(
+    `${BACKEND_URL}/${ApiRoute.COLLECTION}`
+  );
   return collectionSchema.array().parseAsync(data);
 }
 
 export async function fetchCollection(id: string): Promise<Collection> {
-  const data = await axios.get(`${BACKEND_URL}/${ApiRoute.COLLECTION}/${id}`);
+  const { data } = await axios.get<Collection>(
+    `${BACKEND_URL}/${ApiRoute.COLLECTION}/${id}`
+  );
   return collectionSchema.parseAsync(data);
 }
