dd0f4d4a3f3c8af1eaca27a002ae4b2a3f33a502
k5va <k5va@yandex.ru>
2023-03-05 17:04:58 +0500

    Fixes site menu gap

--- src/components/site-menu/site-menu.tsx
+++ src/components/site-menu/site-menu.tsx
@@ -7,7 +7,7 @@ export function SiteMenu(): JSX.Element {
   const { t } = useTranslation();
 
   return (
-    <ul className="grid grid-cols-2 gap-x-3 gap-y-20 text-inherit">
+    <ul className="grid grid-cols-2 gap-3 text-inherit">
       {menuRoutes.map(({ link, text }, index) => (
         <li key={index}>
           <Link href={link}>{t(text)}</Link>
